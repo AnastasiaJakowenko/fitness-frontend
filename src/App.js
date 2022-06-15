8dc925051346b99855ea2d94fbe22b816dcc4de7
niko <npdci@outlook.com>
2022-06-15 14:49:29 +0200

    bild und text für about

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import axios from 'axios';
 import Navigation from './components/Navigation.js';
+import About from './components/About.js';
 function App() {
   return (
+    <>
     <div className="App">
-    
      <Navigation/>
     </div>
+    <div>
+      <About/>
+    </div>
+    </>
   );
 }
 
