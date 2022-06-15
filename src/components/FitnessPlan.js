c36397023ce2fc6149f170a208c436b53dbb497e
luk <luciano_rios@gmx.de>
2022-06-15 16:20:15 +0200

    fitness-plan content added

---
+++ src/components/FitnessPlan.js
@@ -0,0 +1,22 @@
+import "../style/fitnessPlan.css";
+
+function FitnessPlan() {
+  return (
+    <>
+      <div className="fitness-div">
+        <h1 className="fitness-h1">Fitness Plan</h1>
+        <h3 className="fitness-h3">1 Monat</h3>
+        <p className="fitness-p">Attack feet behind the couch destroy couch flop over give attitude hide when guests come over hopped</p>
+        <h3 className="fitness-h3">3 Monate</h3>
+        <p className="fitness-p">Attack feet behind the couch destroy couch flop over give attitude hide when guests come over hopped</p>
+        <h3 className="fitness-h3">1 Jahr</h3>
+        <p className="fitness-p">Attack feet behind the couch destroy couch flop over give attitude hide when guests come over hopped</p>
+        <div className="fitness-button-div">
+          <button className="fitness-button">Register</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default FitnessPlan;
