61406c4896ee1e26263a6798ceb96b3f13ebc5d7
luk <luciano_rios@gmx.de>
2022-06-20 13:52:12 +0200

    fitness-plan-ohne-slideshow

--- src/components/FitnessPlan.js
+++ src/components/FitnessPlan.js
@@ -5,15 +5,16 @@ function FitnessPlan() {
     <>
       <div className="fitness-div">
         <h1 className="fitness-h1">Fitness Plan</h1>
-        <h3 className="fitness-h3">1 Monat</h3>
+        <h3 className="fitness-h3"><i class="fa-solid fa-calendar-day"></i> 1 Monat</h3>
         <p className="fitness-p">Attack feet behind the couch destroy couch flop over give attitude hide when guests come over hopped</p>
-        <h3 className="fitness-h3">3 Monate</h3>
+        <h3 className="fitness-h3"><i class="fa-solid fa-cubes"></i> 3 Monate</h3>
         <p className="fitness-p">Attack feet behind the couch destroy couch flop over give attitude hide when guests come over hopped</p>
-        <h3 className="fitness-h3">1 Jahr</h3>
+        <h3 className="fitness-h3"><i class="fa-regular fa-calendar-days"></i> 1 Jahr</h3>
         <p className="fitness-p">Attack feet behind the couch destroy couch flop over give attitude hide when guests come over hopped</p>
         <div className="fitness-button-div">
-          <button className="fitness-button">Register</button>
+          <button className="fitness-button" ><a href="#">Register</a></button>
         </div>
+        <h1>hola</h1>
       </div>
     </>
   );
