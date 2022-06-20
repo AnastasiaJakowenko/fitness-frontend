3425252212009e5cda7c3aa7d85be232f3995651
np-webdev <89039727+np-webdev@users.noreply.github.com>
2022-06-20 14:50:51 +0200

    np-webdev  about mit main gemerged
    * luk-fontain content added fitness-plan
    
    * fitness-plan content added
    
    * fitness-plan-ohne-slideshow
    
    * Nastja gradient nav (#2)
    
    Co-authored-by: Luciano_R <82583901+luk-fontain@users.noreply.github.com>
    Co-authored-by: AnastasiaJakowenko <89066348+AnastasiaJakowenko@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import axios from 'axios';
 import Navigation from './components/Navigation.js';
 import About from './components/About.js';
+import FitnessPlan from './components/FitnessPlan.js';
+
 function App() {
   return (
     <>
-    <div className="App">
-     <Navigation/>
-    </div>
-    <div>
-      <About/>
-    </div>
+      <div className="App">
+        <Navigation />
+      </div>
+      <div>
+        <About />
+      </div>
+      <div>
+        <FitnessPlan />
+      </div>
     </>
+
   );
 }
 
