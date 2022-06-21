a733956368af182783009d7e10a4acb482c00a64
AnastasiaJakowenko <jakowenko.anastasia1313@gmail.com>
2022-06-21 13:07:37 +0200

    slide show

---
+++ src/components/SlideShow.js
@@ -0,0 +1,32 @@
+import "../style/slide-show.css"
+
+function SlideShow(){
+    return (
+        <>
+         <div className="about__slideshow">
+         <img
+        className="about__img"
+        src="https://bubblingwitheleganceandgrace.com/wp-content/uploads/2015/12/weights.png"
+      ></img>
+      <img
+        className="about__img about__img--slideshow about__img--slideshow1"
+        src="https://www.gym-coaching.de/wp-content/uploads/2017/09/fitnessstudio.jpg"
+      ></img>
+       <img
+        className="about__img about__img--slideshow about__img--slideshow2"
+        src="https://images.unsplash.com/photo-1599058917212-d750089bc07e?ixlib=rb-1.2.1&ixid=MnwxM[…]90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80"
+      ></img>
+      <img
+        className="about__img about__img--slideshow about__img--slideshow3"
+        src="https://images.unsplash.com/photo-1584735935682-2f2b69dff9d2?ixlib=rb-1.2.1&ixid=MnwxM[…]90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+      ></img>
+                     {/* <img className="about__img" src="./pascal.jpg" alt="" />
+                    <img className="about__img about__img--slideshow about__img--slideshow1" src="https://www.gym-coaching.de/wp-content/uploads/2017/09/fitnessstudio.jpg"
+                        alt="">
+                    <img className="about__img about__img--slideshow about__img--slideshow2" src="https://images.unsplash.com/photo-1599058917212-d750089bc07e?ixlib=rb-1.2.1&ixid=MnwxM[…]90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="">
+                    <img className="about__img about__img--slideshow about__img--slideshow3" src="https://bubblingwitheleganceandgrace.com/wp-content/uploads/2015/12/weights.png" alt="">  */}
+                </div>
+        </>
+    )
+} 
+export default SlideShow
