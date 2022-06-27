08461957e274af73a37f96e51315a2e576f08c47
Dawid <dawidmuraszewski@web.de>
2022-06-27 16:25:35 +0200

    login-test-finished

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,40 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import "../style/login.css";
+import axios from 'axios';
+import { useState, useEffect, useRef } from "react";
 
 export const Login = () => {
+    const [isLogin, setIsLogin] = useState([]);
+    const inputRef = useRef();
+    useEffect(() => {
+        const HandleLogin = async () => {
+            //new response
+            const response = await axios.post("http://localhost:4001/login", { inputRef });
+            setIsLogin(response.data);
+        };
+        HandleLogin();
+    }, []);
+
+
     return (
         <div className='loginContainer d-flex flex-column justify-content-center'>
             <h1> Einloggen</h1>
-            <Form className="">
+            <Form method="POST">
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <i class="fa-regular fa-envelope"></i>
-                    < Form.Control type="email" placeholder="Email:" />
+                    < Form.Control name="email" type="email" ref={inputRef} placeholder="Email:" />
                     <Form.Text className="text-muted">
                     </Form.Text>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <i class="fa-solid fa-lock"></i> <Form.Control type="password" placeholder="Password:" />
+                    <i class="fa-solid fa-lock"></i> <Form.Control type="password" placeholder="Password:" name="password" ref={inputRef} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
                 <div className="d-flex justify-content-center">
-                    <Button className="d-flex justify-content-center align-items-center" variant="primary" type="submit">
+                    <Button type="submit" className="d-flex justify-content-center align-items-center" variant="primary" >
                         Login
                     </Button>
                 </div>
