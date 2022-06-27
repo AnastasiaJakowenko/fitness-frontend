33494a289244dab2d863d4b7267de6874ddeee36
niko <npdci@outlook.com>
2022-06-27 11:35:52 +0200

    impressum angefangen

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ import About from "./components/About.js";
 import FitnessPlan from "./components/FitnessPlan.js";
 import Kurse from "./components/Kurse.js";
 import Footer from "./components/Footer.js";
+import Impressum from "./components/Impressum.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
       <div className="App">
         <Navigation />
         <About />
         <FitnessPlan />
         <Kurse />
         <Footer />
+{/*         <Switch>
+        <div ClassName="content">
+          <Route path="/Impressum">
+          <Impressum />
+          </Route>
+          </div>
+          </Switch> */}
       </div>
+    </Router>
   );
 }
 
 export default App;
-
-//kommentar
