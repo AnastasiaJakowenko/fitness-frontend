bce6bc9817cdbae7a2460ca0804cd1a37c7cb820
AnastasiaJakowenko <89066348+AnastasiaJakowenko@users.noreply.github.com>
2022-06-28 09:23:06 +0200

    Dawid: router problem resolved
    * router problems
    
    * Update App.js
    
    Co-authored-by: DawidMura <89196594+DawidMura@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-// import axios from 'axios';
-import Navigation from "./components/Navigation.js";
 import About from "./components/About.js";
 import FitnessPlan from "./components/FitnessPlan.js";
 import Kurse from "./components/Kurse.js";
-import Footer from "./components/Footer.js";
-import Impressum from "./components/Impressum.js";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage.js";
+import { Login } from "./components/Login.js";
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navigation />
-        <About />
-        <FitnessPlan />
-        <Kurse />
-        <Footer />
-{/*         <Switch>
-        <div ClassName="content">
-          <Route path="/Impressum">
-          <Impressum />
-          </Route>
-          </div>
-          </Switch> */}
-      </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/fitness-plan" element={<FitnessPlan />} />
+        <Route path="/kurse" element={<Kurse />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
 
-    </Router>
+    </>
+
   );
 }
 
 export default App;
+
+//kommentar
