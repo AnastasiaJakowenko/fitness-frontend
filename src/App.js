b1597d4ec6c55e41ea1e0c58faf3cb80ec0019cb
DawidMura <89196594+DawidMura@users.noreply.github.com>
2022-06-28 09:20:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-// import axios from 'axios';
-import { Login } from "./pages/login.js";
-import { Home } from "./pages/home.js"
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import { NavLink, Routes, Route, Navigate } from "react-router-dom"
- const App = () => {
+import About from "./components/About.js";
+import FitnessPlan from "./components/FitnessPlan.js";
+import Kurse from "./components/Kurse.js";
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage.js";
+import { Login } from "./components/Login.js";
+function App() {
   return (
-    <div className="App">
-      <Router>
-       
-          <Route path="/" component={Home} />
-          <Route path="/" render={() => <Home />} />
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/fitness-plan" element={<FitnessPlan />} />
+        <Route path="/kurse" element={<Kurse />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
 
-          {/* <Route path="/" element={<Home/>} 
-        /> */}
-           <Route path="/login" component={Login} />
-          <Route path="/login" render={() => <Login />} />
-          
-          
-      
-      </Router>
-    </div>
+    </>
 
   );
 }
 
- ReactDOM.render(<App />, document.getElementById('root'));
 export default App;
 
 //kommentar
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// const Example = () => <p>Example Komponente</p>;
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Route path="/example" component={Example} />
-//       <Route path="/example" render={() => <Example />} />
-//     </Router>
-//   );
-// };
-
