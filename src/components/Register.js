beb7dba339283ec6b9f7cdfb3c06a83400208b81
AnastasiaJakowenko <jakowenko.anastasia1313@gmail.com>
2022-06-28 13:51:01 +0200

    register ist fertig

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,65 +1,75 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import "../style/register.css";
+import '../style/register.css';
 export const Register = () => {
   return (
     <>
-    <div className='register-container'>
-        
-    <h1> Register</h1>
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="text" placeholder="Vorname:" />
-      </Form.Group>
+      <div className="register-container">
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="text" placeholder="Nachname:" />
-      </Form.Group>
-       
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="email" placeholder="Email:" />
-      </Form.Group>
+        <h1> Registrieren</h1>
+        <h3>Persönliche Daten</h3>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <i className="fa-solid fa-user icon"> </i>
+            {/* hier before Vorname und weiter sind 3 Leereplätze, damit es nicht unter icon  liegt */}
+            <Form.Control type="text" placeholder="   Vorname:" /> 
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Control type="password" placeholder="Password:" />
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <i className="fa-solid fa-user icon"></i>
+            <Form.Control type="text" placeholder="   Nachname:" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Control type="password" placeholder="Password bestätigen:" />
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <i className="fa-regular fa-envelope icon"></i>
+            <Form.Control type="email" placeholder="   Email:" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="number" placeholder="Alter:" />
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <i className="fa-solid fa-lock icon"></i>
+            <Form.Control type="password" placeholder="   Password:" />
+          </Form.Group>
 
-     
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <i className="fa-solid fa-triangle-exclamation icon"> </i>
+            <Form.Control type="password" placeholder="   Password bestätigen:" />
+          </Form.Group>
 
-       <h1> Adresse:</h1>
-       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="text" placeholder="Straße:" />
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <i className="fa-solid fa-person icon"></i>
+            <Form.Control type="number" placeholder="   Alter:" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="text" placeholder="Husnummer:" />
-      </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="text" placeholder="Stadt:" />
-      </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="number" placeholder="PLZ:" />
-      </Form.Group>
+          <h3 className="adresseH3"> Adresse:</h3>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <i className="fa-solid fa-road icon"></i>
+            <Form.Control type="text" placeholder="   Straße:" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <i className="fa-solid fa-house-chimney-user icon"></i>
+            <Form.Control type="text" placeholder="   Husnummer:" />
+          </Form.Group>
 
-    </div>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <i className="fa-solid fa-city icon"></i>
+            <Form.Control type="text" placeholder="   Stadt:" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <i className="fa-solid fa-list-ol icon"></i>
+            <Form.Control type="number" placeholder="   PLZ:" />
+          </Form.Group>
+
+
+          <Button variant="primary" type="submit">
+            Registrieren
+          </Button>
+        </Form>
+
+      </div>
     </>
   );
 }
