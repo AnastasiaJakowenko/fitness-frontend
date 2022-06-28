4688743bc5eb83d0ba024b3802c8bce0049eaf08
AnastasiaJakowenko <89066348+AnastasiaJakowenko@users.noreply.github.com>
2022-06-28 11:23:01 +0200

    login außer bordergemacht (#16)

--- src/components/Login.js
+++ src/components/Login.js
@@ -21,17 +21,15 @@ export const Login = () => {
             <h1> Einloggen</h1>
             <Form action="http://localhost:4001/login" method="POST">
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <i class="fa-regular fa-envelope"></i>
-                    < Form.Control name="email" type="email" ref={inputRef} placeholder="Email:" />
+                    <i class="fa-regular fa-envelope icon"></i>
+                    < Form.Control name="email" type="email" ref={inputRef} placeholder="   Email:" />
                     <Form.Text className="text-muted">
                     </Form.Text>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <i class="fa-solid fa-lock"></i> <Form.Control type="password" placeholder="Password:" name="password" ref={inputRef} />
+                    <i class="fa-solid fa-lock icon"></i> <Form.Control type="password" placeholder="   Password:" name="password" ref={inputRef} />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
+        
                 <div className="d-flex justify-content-center">
                     <Button type="submit" className="d-flex justify-content-center align-items-center" variant="primary" >
                         Login
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,17 +1,9 @@
-import Navigation from "../components/Navigation.js";
-import About from "../components/About.js";
-import FitnessPlan from "../components/FitnessPlan.js";
-import Kurse from "../components/Kurse.js";
-import Footer from "../components/Footer.js";
+
 import { Login } from "../components/Login.js"
-export const HomePage = () => {
+export const LoginPage = () => {
     return (
         <>
-            <Navigation />,
-            <About />,
-            <FitnessPlan />,
-            <Kurse />,
-            <Footer />
+        
             <Login />
         </>
     );
