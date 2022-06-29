464b92bbb34e10c3f84c0ba97955d9ffbde16454
AnastasiaJakowenko <jakowenko.anastasia1313@gmail.com>
2022-06-29 13:03:25 +0200

    aa

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,38 @@
 import React from "react";
-import "../style/nav.css"
-// import { render } from "react-dom";
-// import  FontAwesomeIcon  from "@fortawesome/react-fontawesome";
+import "../style/nav.css";
+// import { NavLink } from "react-router-dom";
 function Navigation() {
-    return (
-        <>
-            <ul className="nav">
+  return (
+    <>
+      <ul className="nav">
+        <li className="nav-item home">
+          <a className="nav-link active" href="/">
+            <i className="fa-solid fa-bars"></i>
+          </a>
+        </li>
 
-                <li className="nav-item home">
-                    <a className="nav-link active" href="#"><i class="fa-solid fa-bars"></i></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Register</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Login</a>
-                </li>
+        <div className="rechts">
+          <li className="nav-item">
+            <a className="nav-link" href="/register">
+              Register
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/login">
+              Login
+            </a>
+          </li>
+        </div>
+      </ul>
 
-            </ul>
-        </>
-    )
-
+      {/* <NavLink to="/">Home</NavLink>
+      <NavLink to="/todo-app">Todo App</NavLink>
+      <NavLink to="/kontakt">Kontakt</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/datenschutz">Datenschutz</NavLink>
+      <NavLink to="/extras">Extras</NavLink>
+      <NavLink to="/postslist">Posts list</NavLink> */}
+    </>
+  );
 }
-export default Navigation
-
-
-
-
+export default Navigation;
