474893105fb47577be4571a257b0c20325c8555e
Luciano_R <82583901+luk-fontain@users.noreply.github.com>
2022-06-29 15:58:47 +0200

    luciano: kontakt angefangen

--- src/components/Kontakt.js
+++ src/components/Kontakt.js
@@ -1,9 +1,12 @@
+
 import "../style/kontakt.css";
+
 
 export const Kontakt = () => {
   return (
     <>
       <div className="kontaktcontainer">
+
         <div className="kontaktformular">
           <h1>Kontaktiere uns</h1>
 
@@ -14,26 +17,37 @@ export const Kontakt = () => {
             </label>
             <input type="submit" value="Absenden" />
           </form>
+
         </div>
 
         <div className="kontaktdaten">
           <div>
-          <h5>RUF UNS AN</h5>
-          <p>Tel.: 030 / 123456</p>
+
+            <h5>Rufen Sie uns an</h5>
+
+            <p> Tel.: 030 / 123456</p>
+          </div>
+          <div>
+            <h5>Lage</h5>
+
+            <p>
+              Grupo 5
+              <br />
+              Citystreet 5
+              <br />
+              12345 Berlin
+              <br />
+              Germany
+            </p>
           </div>
 
-          <div>
-          <h5>LAGE</h5>
-          <p>Grupo 5<br/>
-          Citystreet 5<br/>
-          12345 Berlin<br/>
-          Germany</p>
-          </div>
-          
         </div>
-        <div className="map">
+        <div className="Map">
           <img src="https://www2.tuhh.de/zll/wp-content/uploads/placeholder.png"></img>
         </div>
+
+
+
       </div>
     </>
   );
