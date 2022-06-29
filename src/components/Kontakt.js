61bb213abf3f90d1c63a31ee3c25b46cad294b7f
luk <luciano_rios@gmx.de>
2022-06-29 15:50:08 +0200

    kontakt

--- src/components/Kontakt.js
+++ src/components/Kontakt.js
@@ -1,19 +1,48 @@
+import "../style/kontakt.css"
 
 export const Kontakt = () => {
   return (
     <>
-      <h1>KONTAKT</h1>
-      <p>
-        Hier findet ihr uns
-        bild oder google maps?
-        Grupo 5
-        Citystreet 5
-        12345 Berlin
-        Germany
-        social media?
-        eMail: info@grupo5.com
-        Tel.: 030 / 123456
-      </p>
+      <div className="kontaktcontainer">
+        <div className="kontakformular">
+          <h1>Kontaktiere uns</h1>
+          <form>
+            <label>
+              Name:
+              <input type="text" name="name" />
+            </label>
+            <input type="submit" value="Absenden" />
+          </form>
+
+        </div>
+
+        <div className="kontaktdaten">
+          <div>
+            <h5>Rufen Sie uns an</h5>
+
+            <p> Tel.: 030 / 123456</p>
+          </div>
+          <div>
+            <h5>Lage</h5>
+
+            <p>
+              Grupo 5
+              <br />
+              Citystreet 5
+              <br />
+              12345 Berlin
+              <br />
+              Germany
+            </p>
+          </div>
+
+        </div>
+        <div className="Map">
+          <img src="https://www2.tuhh.de/zll/wp-content/uploads/placeholder.png"></img>
+        </div>
+
+
+      </div>
     </>
   );
 }
