4636e79fee80f6dbe08c5cdf4f33097bfb2d0366
np-webdev <89039727+np-webdev@users.noreply.github.com>
2022-07-06 15:18:24 +0200

    pages und nav bearbeitet (#30)

--- src/pages/UserBmiRechnerPage.js
+++ src/pages/UserBmiRechnerPage.js
@@ -1,12 +1,12 @@
 import Navigation from "../components/Navigation.js";
-import UserBmiRechner from "../components/UserBmiRechner.js";
+import Bmi from "../components/Bmi.js";
 import Footer from "../components/Footer.js";
 
 export const UserBmiRechnerPage = () => {
     return (
         <>
             <Navigation />
-            <UserBmiRechner />
+            <Bmi />
             <Footer />
         </>
     );
