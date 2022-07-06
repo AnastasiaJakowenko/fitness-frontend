200303b2bc5fe121be44584ca5d34824ac1d8723
AnastasiaJakowenko <jakowenko.anastasia1313@gmail.com>
2022-07-06 14:31:09 +0200

    kalorienrechner functioniert

--- src/components/KalorienRechner.js
+++ src/components/KalorienRechner.js
@@ -1,67 +1,29 @@
 import React, { useState } from "react";
-import Bmi_diagramm from "./Bmi_diagramm";
 import "../style/kalorienrechner.css"
-const Bmi = () => {
-    const [bmi, setBmi] = useState();
-    const [info, setInfo] = useState();
+const Kalorien = () => {
+    const [kalorienZahl, setkalorienZahl] = useState();
     const [height, setHeight] = useState();
     const [weight, setWeight] = useState();
-    const [recomendations, setRecomendations] = useState()
-    const [bmiColor, setbmiColor] = useState()
     const [age, setAge] = useState()
-    // [655,1 +(9,6 * Number(weight)) + (1,8*Number(height)) - (4,7 * Number(age))]
-    // [Number(weight) / Number(age) / Number(height)] * 10000
-    const handleBmi = () => {
-        // let frauenFormel =(655, 1 + (9, 6 * Number(weight)) + (1, 8 * Number(height)) - (4, 7 * Number(age)))).toFixed(1);
-        // let mannerFormel =
+    const [gender, setGender] = useState()
 
+    const handleChange=(e)=>{
+        setGender(e.target.value)
+    }
+   
+    const handleKalorienZahl = () => {
 
-        let val  ;
-            
-            
-        if (gender === male) {
-           val = mannerFormel 
+        let frauenFormel = (655,1 +(9,6 * Number(weight)) + (1,8*Number(height)) - (4,7 * Number(age))).toFixed(1)
+        let mannerFormel = (66,47 +(13,7 * Number(weight)) + (5*Number(height)) - (6,8 * Number(age))).toFixed(1)
+
+        let val ;
+        if(gender === "Male"){
+            val = mannerFormel
         }else{
-            val = frauenFormel
+            val= frauenFormel
         }
-        setBmi(val);
-        if (val < 18.5) {
-            setRecomendations(
-                <div className="recomendations">
-
-                    <p>Dein Körpergewicht in Kilogramm fällt in Relation zu deiner Größe als untergewichtig aus? Untergewicht kann neben zu geringer Nährstoffaufnahme noch weitere Ursachen haben. Kurzfristig kannst du auf eine ausreichende, gesunde Ernährung achten. Langfristig empfehlen wir, die Ärztin bzw. den Arzt deines Vertrauens aufzusuchen.</p>
-                    <p>Sie können sich bei unsere <a href="/kontakt"> Dietologe</a> melden um eine Beratung zu bekommen. Richtige Ernährung uns Sport können gut auf die Situation beinflussen</p>
-                </div>)
-            setbmiColor("#28b1de")
-            setInfo("Untergewicht");
-        } else if (val > 18.5 && val <= 24.9) {
-            setRecomendations(
-                <div className="recomendations">
-
-                    <p>Gratulation, du hast einiges richtig gemacht – der BMI Rechner zeigt dir ein super Ergebnis ! Achte darauf, dein Gewicht durch eine ausgewogene Ernährung und Bewegung in diesem Bereich zu halten.</p>
-                    <p>Hier kannst du für sich interessante <a href="/kurse">Kurse</a> auswählen. Wir werden uns auf persönliches Treffen freuen!</p>
-                </div>)
-            setbmiColor("#25aa1e")
-            setInfo("Normalgewicht");
-        } else if (val > 24.9 && val < 30) {
-            setRecomendations(
-                <div className="recomendations">
-
-                    <p>Grundsätzlich gilt: Übergewicht laut BMI Rechner ist nicht gleich Übergewicht und noch lange nicht gleichbedeutend mit ungesund. Auch sehr trainierte Menschen mit einem hohen Muskelanteil haben häufig einen hohen Body Mass Index, daher ist eine Körperfettanalyse in solchen Fällen aussagekräftiger. </p>
-                    <p>Melde dich bei unsere <a href="/kontakt"> Fitnesstrainer</a>, um eine Beratung zu bekommen, oder schau dir unsere <a href="/kurse">Kurse</a> an. Wir werden uns auf persönliches Treffen freuen!</p>
-                </div>)
-            setbmiColor("#dc9835")
-            setInfo("Übergewicht");
-        } else {
-            setRecomendations(
-                <div className="recomendations">
-
-                    <p>Sollte dein Wert als adipös Adipositas eingestuft werden, raten wir dir, mit deiner Ärztin bzw. deinem Arzt oder einer Beraterin bzw. einem Berater im Ernährungsbereich zu sprechen. So kann die Art deines Übergewichtes (Fettleibigkeit, Präadipositas, Adipositas Grad i, Adipositas Grad ii oder Adipositas Grad iii) richtig analysiert werden. Ab einem weit erhöhten BMI ist es sehr wichtig für deine Gesundheit, dein Gewicht zu regeln – der Rechner kann dabei der erste Schritt für eine gesunde Zukunft sein.</p>
-                    <p>Wir bieten auch die <a href="/kurse"> Kurse</a> für Anfänger an, damit du langsam und gesund ein neues und gesundes Leben mit G5 Team anfangen könntest. Unser Team wird dich von Anfang an unterstützen und motivieren.</p>
-                </div>)
-            setbmiColor("#e54915")
-            setInfo("Adipositas");
-        }
+        setkalorienZahl(val);
+   
     };
     return (
         <div className="container">
@@ -80,13 +42,14 @@ const Bmi = () => {
                 />
 
             </div>
+            {/* hier sind radio bottons die als icons aussehen . wir haben zeile 8 useState gender gemacht und in zeile 10 ziehen wir der user auswahl(male/female) um das weiter füe formel zu benutzen */}
             <div className="input_container2">
                 <label>
-                    <input className="input_radio" type="radio" value="Male" name="gender" />
+                    <input className="input_radio" type="radio" value="Male"   onChange={handleChange}name="gender" />
                     <i class="fa-solid fa-person "></i>
                 </label>
                 <label>
-                    <input className="input_radio" type="radio" value="Female" name="gender" />
+                    <input className="input_radio" type="radio" value="Female"   onChange={handleChange}name="gender" />
                     <i class="fa-solid fa-person-dress"></i>
                 </label>
 
@@ -96,18 +59,19 @@ const Bmi = () => {
                     placeholder="Gewicht in kg"
                 />
             </div>
+            {/* hier ist button */}
             <div className="bmi_button_container">
-                <button className="bmi_button" onClick={handleBmi}>Berechnen</button>
+                <button className="bmi_button" onClick={handleKalorienZahl}>Berechnen</button>
             </div>
-            <div className="animation">
+            {/* hier unsere arrow */}
+            <div className="animation"></div>
+            
+            <div className="ergebnisse_container" >
 
+                <h1 className="ergebnisse">Deine tägliche Kalorienbedarf ist {kalorienZahl} Kalorien pro Tag</h1>
+              
             </div>
-            <div className="ergebnisse_container" style={{ color: bmiColor }}>
-
-                <h1 className="ergebnisse">Deine tägliche Kalorienbedarf ist {bmi} Kalorien pro Tag</h1>
-                {/* <h2 className="ergebnisse">{info}</h2> */}
-            </div>
-            {/* <h2 className="ergebnisse_info">{info}</h2> */}
+           
             <h2>Welche Rolle spielt der Kalorienverbrauch beim Abnehmen?</h2>
             <p> Eine große, denn der Grundumsatz macht 50 bis 70 Prozent des gesamten Energiebedarfs aus.
 
@@ -123,4 +87,4 @@ const Bmi = () => {
     );
 };
 
-export default Bmi;
+export default Kalorien;
