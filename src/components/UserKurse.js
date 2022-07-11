a68e7774104ad65c645e87b21dca09d6f15c4162
luk <luciano_rios@gmx.de>
2022-07-11 12:04:07 +0200

    vor der pause

--- src/components/UserKurse.js
+++ src/components/UserKurse.js
@@ -1,11 +1,19 @@
 import React from "react";
+import "../style/userkurse.css";
 
 function UserKurse() {
     return (
         <>
 
-            <div className="UserKurse">
-                <h1>User Kurse</h1>
+            <div className="userKurse">
+                <h1>Kurse</h1>
+                <div className="userKurseIcons">
+                    <div className="userKurseIconsRechts">
+                        <i class="fa-regular fa-calendar-days"></i>
+                        <i class="fa-solid fa-circle-arrow-left"></i>
+                        <i class="fa-solid fa-circle-arrow-right"></i>
+                    </div>
+                </div>
             </div>
 
         </>
