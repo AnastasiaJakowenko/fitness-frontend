ad1499d925493bd06a05d04325b8d57884916583
AnastasiaJakowenko <jakowenko.anastasia1313@gmail.com>
2022-07-15 10:47:38 +0200

    ups

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -5,7 +5,7 @@ import "../style/tools.css";
 function Tools() {
     return (
       <>
-           <h1>Tools</h1>
+           <h1 id="tools_h1">Tools</h1>
         <a href="/bmirechner" className="a_tools"><div className="bmi">
             <p className="tools_p">BMI Rechner</p>
         </div></a>
