f766c3f562e22fa2b4addabb61330fb53176211b
AnastasiaJakowenko <jakowenko.anastasia1313@gmail.com>
2022-07-15 09:49:18 +0200

    mediaquerry about

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,16 @@ import "../style/about.css";
 
 function About() {
   return (
-    <>
 
+    <div className="about_container">
+   
+      <div>
       <img
         className="studio"
         src="https://images.unsplash.com/photo-1579758629938-03607ccdbaba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt=""
       ></img>
+      </div>
+      
       <div>
         <h1 className="about_h1">Fitnessstudio G5</h1>
         <p className="about_p">
@@ -19,7 +23,7 @@ function About() {
           Magdeburg fit – und gerne auch dich!
         </p>
       </div>
-    </>
+    </div>
   );
 }
 
