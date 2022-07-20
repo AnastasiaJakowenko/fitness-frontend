bb1f3d686837be05a5347a294c73d77f8bffbfaf
AnastasiaJakowenko <jakowenko.anastasia1313@gmail.com>
2022-07-20 14:53:50 +0200

    siedebar gut

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,27 +7,28 @@ function Sidebar() {
     <Menu className="menu-sidebar">
       <a className="menu-item" href="/">
         <i className="fa-solid fa-house"></i>
+        <p>Home</p>
       </a>
       <a className="menu-item" href="/userkurse">
-        <i className="fa-solid fa-person-running"></i>
+        <i className="fa-solid fa-person-running"></i><p>Kurse</p>
       </a>
       <a className="menu-item" href="/usersportgeraete">
-        <i className="fa-solid fa-dumbbell"></i>
+        <i className="fa-solid fa-dumbbell"></i><p>Sportgeräte</p>
       </a>
       <a className="menu-item" href="/bmirechner">
-        <i className="fa-solid fa-calculator"></i>
+        <i className="fa-solid fa-calculator"></i><p>BMI-Rechner</p>
       </a>
       <a className="menu-item" href="/kalorienrechner">
-        <i className="fa-solid fa-burger"></i>
+        <i className="fa-solid fa-burger"></i><p>Kalorien-Rechner</p>
       </a>
       <a className="menu-item" href="/uservertraginfos">
-        <i className="fa-solid fa-file-contract"></i>
+        <i className="fa-solid fa-file-contract"></i><p>Vertgragsinfo</p>
       </a>
       <a className="menu-item" href="/usereinstellungen">
-        <i className="fa-solid fa-gear"></i>
+        <i className="fa-solid fa-gear"></i><p>Einstellungen</p>
       </a>
       <a className="menu-item" href="/userpanel">
-        <i className="fa-solid fa-user"></i>
+        <i className="fa-solid fa-user"></i><p>User</p>
       </a>
     </Menu>
   );
